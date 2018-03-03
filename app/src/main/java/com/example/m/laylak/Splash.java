c73a91dbb92615bcf8fd5a613e475f24b8767624
AsmaaMohamed <smasimo81@gmail.com>
2018-03-03 14:39:43 +0200

    Initial commit

---
+++ app/src/main/java/com/example/m/laylak/Splash.java
@@ -0,0 +1,28 @@
+package com.example.m.laylak;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+
+
+        int secondsDelayed = 1;
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent(Splash.this, LoginActivity.class));
+                finish();
+            }
+        }, secondsDelayed * 3000);
+    }
+    }
+
