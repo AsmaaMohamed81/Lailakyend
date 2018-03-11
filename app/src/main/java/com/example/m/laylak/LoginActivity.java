d5f447a5bda2547d6e027db9b1e44041c53c38e2
AsmaaMohamed <smasimo81@gmail.com>
2018-03-11 11:55:10 +0200

    ff

--- app/src/main/java/com/example/m/laylak/LoginActivity.java
+++ app/src/main/java/com/example/m/laylak/LoginActivity.java
@@ -1,13 +1,40 @@
 package com.example.m.laylak;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+    Button login;
+
+  TextView regist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        login=findViewById(R.id.log);
+        regist=findViewById(R.id.regist);
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(LoginActivity.this,AlbumsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        regist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(LoginActivity.this,signup.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/m/laylak/signup.java
@@ -0,0 +1,13 @@
+package com.example.m.laylak;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup);
+    }
+}
