d313432a68521831ac0dd317dc6402381426b35c
Asmaa <smasimo81@gmail.com>
2019-06-19 11:15:06 +0200

    asmaa

--- app/src/main/java/com/Alatheer/Projects/lailaky/Activites/CoverActivity.java
+++ app/src/main/java/com/Alatheer/Projects/lailaky/Activites/CoverActivity.java
@@ -1,10 +1,9 @@
 package com.Alatheer.Projects.lailaky.Activites;
 
-import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
--- app/src/main/java/com/Alatheer/Projects/lailaky/Activites/confirmEmail_ForgetPass.java
+++ app/src/main/java/com/Alatheer/Projects/lailaky/Activites/confirmEmail_ForgetPass.java
@@ -1,23 +1,20 @@
 package com.Alatheer.Projects.lailaky.Activites;
 
-import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
+import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.Alatheer.Projects.lailaky.ApiServices.Api;
-import com.Alatheer.Projects.lailaky.ApiServices.Preferences;
 import com.Alatheer.Projects.lailaky.ApiServices.Services;
 import com.Alatheer.Projects.lailaky.Models.UserModel;
 import com.Alatheer.Projects.lailaky.R;
